e1f89f25c57864cfbfa5c25c5954f78ee976a6c3
psajith <SAJgrid@2025>
2025-07-11 08:15:19 +0530

    Visa Questions Covered - Session 1

---
+++ src/main/java/org/griddynamics/AdditionWithOutOperatorBitManupilation.java
@@ -0,0 +1,17 @@
+package org.griddynamics;
+
+public class AdditionWithOutOperatorBitManupilation {
+    public static void main(String args[]){
+        int x=10;
+        int y=12;
+        System.out.println("Sum :: "+getAnswer(x,y));
+    }
+    public static int getAnswer(int x, int y){
+        while(y!=0){
+            int carry=x&y;
+            x=x^y;
+            y=carry<<1;
+        }
+        return x;
+    }
+}
---
+++ src/main/java/org/griddynamics/ReversedString.java
@@ -0,0 +1,23 @@
+package org.griddynamics;
+
+public class ReversedString {
+    public static void main(String args[]) {
+        String input = "My Name is John Honai. I am a don ";
+        System.out.println("The reversed string is : " + getItReversed(input));
+    }
+
+    public static String getItReversed(String input) {
+        String[] splitInput = input.split("\\.");
+        String[] eachWord = splitInput[0].trim().split("\\s+");
+        StringBuilder result = new StringBuilder();
+        for (int j = 0; j <splitInput.length; j++) {
+            for (int i = eachWord.length - 1; i >= 0; i--) {
+                result.append(eachWord[i]);
+                result.append(" ");
+            }
+
+            if (splitInput.length - 1 != j) result.append(".");
+        }
+        return result.toString();
+    }
+}
