2a11ff0c4127d9663f4e35d3b746b353235d8eec
psajith <SAJgrid@2025>
2025-07-11 11:34:57 +0530

    Visa Questions Covered - added more

---
+++ src/main/java/org/griddynamics/AddDelimitertoArrayList.java
@@ -0,0 +1,27 @@
+package org.griddynamics;
+
+import javax.swing.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AddDelimitertoArrayList {
+    public static void main(String[] args){
+        List<Integer> test=new ArrayList<>();
+        for(int i=0;i<=100;i++){
+            test.add(i);
+        }
+//        StringBuilder ans= new StringBuilder();
+//        for(int i=0;i<=100;i++){
+//            ans.append(test.get(i));
+//            if(i<test.size()-1){
+//                ans.append(",");
+//            }
+//        }
+//        System.out.println(ans.substring(0,10));
+        String testAnswer= test.stream()
+                .map(String::valueOf)
+                .collect(Collectors.joining(","));
+    }
+}
