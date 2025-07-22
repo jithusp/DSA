5195665e8d097687dd48169337ded596ec39cb9c
psajith <SAJgrid@2025>
2025-07-22 13:22:36 +0530

    Merge remote-tracking branch 'origin/main'

--- src/main/java/org/griddynamics/july17/UnmodifiableImmutableObjects.java
+++ src/main/java/org/griddynamics/july17/UnmodifiableImmutableObjects.java
@@ -1,8 +1,10 @@
 package org.griddynamics.july17;
 import java.util.*;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.stream.Collectors;
-// Dont use objects as mutable
-// handle deletion, with concurrent modification exception
+// Dont use objects as mutable - copy of the mutable objects to initialize immutable objects, then Using CopyOnArrayList
+// handle deletion, with concurrent modification exception -done
+
 public class UnmodifiableImmutableObjects {
         public static class MutablePerson {
             private String name;
@@ -57,9 +59,17 @@ public class UnmodifiableImmutableObjects {
 //List<String>qwe=Collections.unmodifiableList(poi);
 
         public static void main(String[] args) {
-            List<MutablePerson> input = new ArrayList<>();
+            List<MutablePerson> input = new CopyOnWriteArrayList<>();
             input.add(new MutablePerson("Alice", 30));
             input.add(new MutablePerson("Bob", 25));
+            try {
+                for (MutablePerson person : input) {
+                    System.out.println("Visiting: " + person);
+                    input.remove(person);
+                }
+            } catch (ConcurrentModificationException e) {
+                System.out.println("Caught ConcurrentModificationException!");
+            }
             PersonService service = new PersonService(input);
             List<Person> ans = service.getAllPeople();
             System.out.println("Immutable People List:");
